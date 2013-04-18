026aa3f12cced0f093d5806c845f1c7457e688b3
Xiaomin Wei <weixiaomin.seu@gmail.com>
2013-04-18 17:15:10 -0400

    add same tree (java and cpp version)

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,4 +1,5 @@
 /**
+ * Q58: Length of Last Word
  * Given a string s consists of upper/lower-case alphabets and empty space 
  * characters ' ', return the length of last word in the string.
  * If the last word does not exist, return 0.
